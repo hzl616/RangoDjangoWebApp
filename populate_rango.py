ad29052d62eace308273906f1eb1a571ec2e1fe8
hzl616 <suesskaninchen@gmail.com>
2021-04-25 13:36:27 +0100

    ChapterSix: Add category.html and generate data driven web content

--- populate_rango.py
+++ populate_rango.py
@@ -11,25 +11,25 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
+         'url': 'http://docs.python.org/3/tutorial/', 'views': 199},
         {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
+         'url': 'http://www.greenteapress.com/thinkpython/', 'views': 500},
         {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}
+         'url': 'http://www.korokithakis.net/tutorials/python/', 'views': 40}
     ]
     django_pages = [
         {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
+         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': 5},
         {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
+         'url': 'http://www.djangorocks.com/', 'views': 58},
         {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'}
+         'url': 'http://www.tangowithdjango.com/', 'views': 42}
     ]
     other_pages = [
         {'title': 'Bottle',
-         'url': 'http://bottlepy.org/docs/dev/'},
+         'url': 'http://bottlepy.org/docs/dev/', 'views': 14},
         {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}
+         'url': 'http://flask.pocoo.org', 'views': 11}
     ]
     cats = {
         'Python': {'pages': python_pages,
--- rango/urls.py
+++ rango/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rango import views
+
 app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
-    path('media/', views.media, name='media')
+    path('media/', views.media, name='media'),
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
 ]
