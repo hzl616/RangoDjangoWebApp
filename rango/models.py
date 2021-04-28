5162c5dc126706fcf3d43e6173a643813a4af156
hzl616 <suesskaninchen@gmail.com>
2021-04-28 11:19:30 +0100

    ChapterSeven: Update views.py to include add_category and add_page functionality. Added corresponding html templates.

--- rango/models.py
+++ rango/models.py
@@ -1,29 +1,32 @@
 from django.db import models
-from django.utils.text import slugify
-
-slug = models.SlugField(unique=True)
+from django.template.defaultfilters import slugify
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LEN = 128
+
+    name = models.CharField(max_length=NAME_MAX_LEN, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
     class Meta:
-        verbose_name_plural = 'Categories'
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
 
 
 class Page(models.Model):
+    PAGE_MAX_LEN = 128
+    URL_MAX_LEN = 200
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=PAGE_MAX_LEN)
     url = models.URLField()
     views = models.IntegerField(default=0)
 
