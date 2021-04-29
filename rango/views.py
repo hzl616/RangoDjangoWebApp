f4494f21f656eebc677351f1b87b1650f5af2d77
hzl616 <80228762+hzl616@users.noreply.github.com>
2021-04-30 00:29:43 +0100

    Update views.py

--- rango/views.py
+++ rango/views.py
@@ -8,7 +8,7 @@ def index(request):
 
 
 def about(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict = {'boldmessage': 'This tutorial has been put together by Hazel'}
     return render(request, 'rango/about.html', context=context_dict)
 
 
